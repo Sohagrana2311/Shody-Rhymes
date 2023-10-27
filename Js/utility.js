87543cefca8acf56ad9bfe334708a635685af70d
unknown <sohagrana2311@gmail.com>
2023-10-27 19:32:05 +0600

    navber-creat

---
+++ Js/utility.js
@@ -0,0 +1,11 @@
+function sohag() {
+    const menuBar= document.getElementById('navber')
+    
+        menuBar.classList.add("active");
+    
+}
+
+function close(){
+    const menuBar= document.getElementById('navber')
+    menuBar.classList.add("close");
+}
